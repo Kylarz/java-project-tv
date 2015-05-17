0c0ebf479f3f953ca3ceeca28c7bcf18d187da55
Symon Gonthiez <seymonx@gmail.com>
2015-05-17 14:15:41 +0200

    Changelogs :
    - Bug de remplissage résolu
    - Mise en place de l'affichage faites
    -Superposition codée, gérée, et affichée
    
    Le projet touche à sa fin, le logger reste à implanter ainsi que le reste des commentaires du codes

--- src/main/java/com/mycompany/tele/Application.java
+++ src/main/java/com/mycompany/tele/Application.java
@@ -14,6 +14,7 @@ public class Application
                                   // déclaration d'un tableau 'Grilletele' de String
         ProgrammeMere Tabprog[] = new ProgrammeMere [50];
         int Grilletele[] = new int [24];
+        String Tabnom[] = new String [50];
         
         
                           // Le tableau Grilletele prend 24 emplacements de boolean. 
@@ -24,10 +25,10 @@ public class Application
         
 // ---------------------------------------- Zone d'ajout des programmes ------------------------------------------------
         
-        programmeList.add("fiction, 21, 2, A.I., 2001, Steven Spielberg, oui");
+        programmeList.add("fiction, 21, 2, Jurrasic World, 2001, Steven Spielberg, oui");
         programmeList.add("divertissement, 12, toto, titi");
-        programmeList.add("reportage, 0, 3, test, culturel");
-        programmeList.add("reportage, 2, 3, test, monde");
+        programmeList.add("reportage, 0, 3, Enquête d'action, culturel");
+        programmeList.add("reportage, 2, 3, Au coeur d'une friterie, monde");
         programmeList.add("FICTION, 10, 2, Interstellar, 2015, Christopher Nolan, OUI");
         programmeList.add("divertiSSement, 12, Vendreditoutestpermis, Arthur");
         
@@ -42,24 +43,28 @@ public class Application
             String[] TSplitted = programmeList.get(i).split(",");
             if (TSplitted[0].equalsIgnoreCase("Divertissement")) {              // si le premier champ est équivalent à "Divertissement", alors on ajoute ce programme dans la liste des divertissements. 
                Tabprog[i]= new Divertissement("Divertissement", Integer.parseInt(TSplitted[1].trim()),TSplitted[2],TSplitted[3]);
-               for (int j = Grilletele[Tabprog[i].heure]; j < Grilletele[Tabprog[i].heure]+Grilletele[Tabprog[i].duree]; j++) {
+               
+               for (int j = Tabprog[i].heure; j < Tabprog[i].heure+Tabprog[i].duree; j++) {
+                   Tabnom[j]= Tabprog[i].nom;
                    Grilletele[j]=Grilletele[j]+1;
                }
                }
-               /* for j=heuredebut, j<heuredebut+duree, j++ Grille[j]=1;*/
+               
             
             
             
             if (TSplitted[0].equalsIgnoreCase("Fiction")) {                     // si le premier champ est équivalent à "Fiction", alors on ajoute ce programme dans la liste des fictions. 
                 Tabprog[i]=new Fiction("Fiction", Integer.parseInt(TSplitted[1].trim()), Short.parseShort(TSplitted[2].trim()),TSplitted[3], Short.parseShort(TSplitted[4].trim()), TSplitted[5], Boolean.parseBoolean(TSplitted[6].trim()));
-                for (int j = Grilletele[Tabprog[i].heure]; j < Grilletele[Tabprog[i].heure]+Grilletele[Tabprog[i].duree]; j++) {
+                for (int j = Tabprog[i].heure; j < Tabprog[i].heure+Tabprog[i].duree; j++) {
+                    Tabnom[j]= Tabprog[i].nom;
                    Grilletele[j]=1;
             }
                 }
             
             if (TSplitted[0].equalsIgnoreCase("Reportage")) {                   // si le premier champ est équivalent à "Reportage", alors on ajoute ce programme dans la liste des reportages. 
                 Tabprog[i]=new Reportage("Reportage", Integer.parseInt(TSplitted[1].trim()), Short.parseShort(TSplitted[2].trim()),TSplitted[3], TSplitted[4]);
-                for (int j = Grilletele[Tabprog[i].heure]; j < Grilletele[Tabprog[i].heure]+Grilletele[Tabprog[i].duree]; j++) {
+                for (int j = Tabprog[i].heure; j < Tabprog[i].heure+Tabprog[i].duree; j++) {
+                   Tabnom[j]= Tabprog[i].nom;
                    Grilletele[j]=Grilletele[j]+1;
                         }   // la méthode .trim() permet, entre deux champs, de ne pas prendre en compte les espaces. 
                              }
@@ -68,14 +73,29 @@ public class Application
             
         
             }
+        
+        System.out.println("Voici le programme TV de la journée :");
         for (int y = 0; y< Grilletele.length;y++) {
             
-            System.out.println(Grilletele[y]);
-                  
-                        
+            if (Tabnom[y] == null) {
+                System.out.println(" Ce créneau est vide " + y + "h " + Grilletele[y]);
+            }
+            else if (Tabnom[y] != null) {
+                
+            
+            System.out.println(Tabnom[y] +" Est diffusé dans le créneau " + y + "h " + Grilletele[y]);
+  
                     }
-                } 
+            
+            if (Grilletele[y] == 2) {
+                System.out.println("SUPERPOSITION DETECTED ERROR : Il y'a une superposition de programme !");
+            }
+        
         }
+        
+        
+        }
+}
 
                 // Après vérification de l'énoncé, seul les programmes de types divertissement et fiction peuvent se croiser
              
